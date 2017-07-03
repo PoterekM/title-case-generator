3891fb27c98d86caa98caf06c6511a04df99e69d
Michelle Poterek and Rena Rallis <michelle@>
2017-07-03 10:59:08 -0700

    add delimiter space to articles to exclude from upper case title

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -2,25 +2,22 @@
 
 class TitleCaseGenerator
 {
-    // function makeTitleCase($input_title)
-    // {
-    // $input_array_of_words = explode(" ", $input_title);
-    // $output_titlecased = array();
-    // foreach ($input_array_of_words as $word) {
-    //     array_push($output_titlecased, ucfirst($word));
-    // }
-
     function makeTitleCase($input_title)
     {
+    $input_array_of_words = explode(" ", $input_title);
+    $output_titlecased = array();
+
     $str = ucwords($input_title);
-    $exclude = 'a,an,the,for,and,nor,but,or,yet,so,such,as,at,around,by,after,along,for,from,of,on,to,with,without';
-    $excluded = explode(",",$exclude);
+    $excluded = array(' a ', ' an ', ' the ', ' for ', ' and ', ' nor ', ' but ', ' or ', ' yet ', ' so ', ' such ', ' as ', ' at ', ' around ', ' by ', ' after ', ' along ', ' for ', ' from ', ' of ', ' on ', ' to ', ' with ', ' without ', ' is ');
     foreach($excluded as $noCap){$str = str_replace(ucwords($noCap),strtolower($noCap),$str);}
     return ucfirst($str);
+    foreach ($input_array_of_words as $word) {
+        array_push($output_titlecased, ucfirst($word));
     }
-
-    // return implode(" ", $output_titlecased);
-    // }
+    return implode(" ", $output_titlecased);
 }
+
+    }
+// }
 
 ?>
